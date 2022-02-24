75dfa590f1f029b928134dd658e39f6cefb82fae
Alex Widener <awidener3@gmail.com>
2022-02-23 19:28:31 -0500

    created reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,22 +1,24 @@
 const { Schema, model } = require('mongoose');
 
-// TODO: Schema
-// reactionId
-// Use Mongoose's ObjectId data type
-// Default value is set to a new ObjectId
-
-// reactionBody
-// String
-// Required
-// 280 character maximum
-
-// username
-// String
-// Required
-
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
+const reactionId = new Schema({
+	reactionId: {
+		type: Schema.Types.ObjectId,
+		default: () => new Types.ObjectId(),
+	},
+	reactionBody: {
+		type: String,
+		required: true,
+		maxlength: 280,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		get: (value) => value.toDateString(),
+	},
+});
 
 module.exports = reactionSchema;
